a2f9871ade50b2025108f937a273d09956bfe188
cedricklubin <c1lubin@student.bridgew.edu>
2021-12-05 18:56:44 -0500

    Initial commit

---
+++ src/main/java/GeomTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.awt.*;
+
+public class GeomTest {
+
+    @Test
+    public void testDist(){
+        var p1 = new Point(0,3);
+        var p2 = new Point(4,0);
+        assert BasicGeom.calcDistance(p1,p2) == 5;
+        assert true;
+
+    }
+
+    @Test
+    public void testZeroDist(){
+        var p1 = new Point(4,4);
+        var p2 = new Point(4,4);
+        assert BasicGeom.calcDistance(p1,p2) == 0;
+    }
+
+}
